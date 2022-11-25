372c3bbf6b7becab7ac53f9b24cb39267e05867d
lhp9-tg <thomas.gouel@gmail.com>
2022-11-25 18:26:08 +0100

    Mise en place de la lib Masonry

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+i = 1;
+
+function fillTheGrid () {
+    var newDiv = document.createElement("div");
+    var width = Math.round((Math.random(3))+1);
+    var height = Math.round((Math.random(3))+1);
+
+    newDiv.classList = 'grid-item grid-item--width'+width+' grid-item--height'+height+'';
+    newDiv.style.background = 'url(https://picsum.photos/'+(width*300)+'/'+(height*100)+'?random=' + i + ')'
+
+ 
+    document.querySelector('.grid').appendChild(newDiv);
+    i++
+}
+window.addEventListener('wheel', () => fillTheGrid())
