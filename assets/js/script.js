5b58a373143ff11b3f6739febd31b8897373a7ae
lhp9-tg <thomas.gouel@gmail.com>
2022-11-28 11:12:18 +0100

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
+
+//infinite scroll
+
 i = 1;
 
-function fillTheGrid () {
+function fillTheGrid() {
+
     var newDiv = document.createElement("div");
-    var width = Math.round((Math.random(3))+1);
-    var height = Math.round((Math.random(3))+1);
 
-    newDiv.classList = 'grid-item grid-item--width'+width+' grid-item--height'+height+'';
-    newDiv.style.background = 'url(https://picsum.photos/'+(width*300)+'/'+(height*100)+'?random=' + i + ')'
+    var width = Math.round((Math.random(3)) + 1);
+    var height = Math.round((Math.random(3)) + 1);
 
- 
+    newDiv.classList = 'grid-item grid-item--width' + width + ' grid-item--height' + height + '';
+    newDiv.style.background = 'url(https://picsum.photos/' + (width * 300) + '/' + (height * 100) + '?random=' + i + ')'
+
     document.querySelector('.grid').appendChild(newDiv);
     i++
+
+    var elem = document.querySelector('.grid');
+    var msnry = new Masonry(elem, {
+        // options
+        itemSelector: '.grid-item',
+    });
 }
+
 window.addEventListener('wheel', () => fillTheGrid())
+
+
